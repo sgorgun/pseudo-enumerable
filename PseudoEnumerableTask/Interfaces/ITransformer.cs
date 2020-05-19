a91a97108805dd06d3d323a09cc60814d769f3f0
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-19 09:31:34 +0000

    add-transforemer

---
+++ PseudoEnumerableTask/Interfaces/ITransformer.cs
@@ -0,0 +1,17 @@
+﻿namespace PseudoEnumerableTask.Interfaces
+{
+    /// <summary>
+    /// Defines a generalized transformation an object from one type to another type.
+    /// </summary>
+    /// <typeparam name="TSource">The type of object that is to be converted. This type parameter is contravariant. </typeparam>
+    /// <typeparam name="TResult">The type the input object is to be converted to. This type parameter is covariant. </typeparam>
+    public interface ITransformer<in TSource, out TResult>
+    {
+        /// <summary>
+        /// Represents a method that converts an object from one type to another type.
+        /// </summary>
+        /// <param name="obj">The object to convert.</param>
+        /// <returns>The TResult that represents the converted TSource.</returns>
+        TResult Transform(TSource obj);
+    }
+}
