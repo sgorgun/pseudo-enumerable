3a3ed2566ff5570cb27f2dd02e958c42e3105bb0
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 18:31:56 +0000

    add-adaptee-class

---
+++ IEEE754FormatTask/DoubleExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IEEE754FormatTask
+{
+    public static class DoubleExtension
+    {
+        //TODO: See your solution of this task https://gitlab.com/epam-autocode-tasks/ieee754
+        
+        /// <summary>
+        /// Returns a string representation of a double type number
+        /// in the IEEE754 (see https://en.wikipedia.org/wiki/IEEE_754) format.
+        /// </summary>
+        /// <param name="number">Input number.</param>
+        /// <returns>A string representation of a double type number in the IEEE754 format.</returns>
+        public static string GetIEEE754Format(this double number)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
