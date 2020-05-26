4db32edd4daa1d0d85f05463f3270bb416846c08
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 18:44:06 +0000

    Upload New File

---
+++ PseudoEnumerableTask.Tests/Comparers/StringByLengthComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PseudoEnumerableTask.Tests.Comparers
+{
+    public class StringByLengthComparer : IComparer<string>
+    {
+        public int Compare(string x, string y) => throw new NotImplementedException();
+    }
+}
