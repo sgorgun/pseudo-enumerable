cd13aaaae2bcf8a12ace88af9dde4d9a1cb90af4
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 18:30:34 +0000

    add-adaptee-class

---
+++ ContainsDigitPredicate/ContainsDigitFilter.cs
@@ -0,0 +1,17 @@
+﻿namespace ContainsDigitPredicate
+{
+    //TODO: See your solution of this task https://gitlab.com/epam-autocode-tasks/int-array-filter
+    public class ContainsDigitValidator
+    {
+        //TODO: Add code here if necessary or delete this comment.
+        public int Digit
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+        public bool Verify(int value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
