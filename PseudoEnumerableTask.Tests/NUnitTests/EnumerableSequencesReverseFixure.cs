26f1b25d251a64bf9e510229b66afe44d5991294
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 18:45:21 +0000

    Upload New File

---
+++ PseudoEnumerableTask.Tests/NUnitTests/EnumerableSequencesReverseFixure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PseudoEnumerableTask.Tests.NUnitTests
+{
+    [TestFixture(new[] {"Beg", null, "Life", "I", "i", "I", null, "To"},
+        new[] {"To", null, "I", "i", "I", "Life", null, "Beg"},
+        TypeArgs = new Type[] {typeof(string)})]
+    [TestFixture(new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        new[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+        TypeArgs = new Type[] {typeof(int)})]
+    public class EnumerableSequencesReverseTestFixture<T>
+    {
+        private readonly T[] source;
+        private readonly T[] expected;
+
+        public EnumerableSequencesReverseTestFixture(T[] source, T[] expected)
+        {
+            this.expected = expected;
+            this.source = source;
+        }
+
+        [Test]
+        public void ReverseTest() => CollectionAssert.AreEqual(source.Reverse(), expected);
+    }
+}
