5dc3aa73a333987155357d87243fccbc82a0be9a
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-26 18:43:50 +0000

    Upload New File

---
+++ PseudoEnumerableTask.Tests/Comparers/IntegerByAbsComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PseudoEnumerableTask.Tests.Comparers
+{
+    public class IntegerByAbsComparer : IComparer<int>
+    {
+        public int Compare(int x, int y) => throw new NotImplementedException();
+    }
+}
