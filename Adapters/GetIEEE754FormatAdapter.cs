1753ee1c9a56e263fabe7f2411ec6513d00520cd
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-17 16:23:27 +0000

    Upload New File

---
+++ Adapters/GetIEEE754FormatAdapter.cs
@@ -0,0 +1,14 @@
+﻿using PseudoEnumerableTask.Interfaces;
+
+namespace Adapters
+{
+    public class GetIEEE754FormatAdapter : ITransformer<double, string>
+    {
+        //TODO: Add necessary code and remove this comment.
+        
+        public string Transform(double obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
