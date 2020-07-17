e44c63d8efe00cd2f825ec4f324789decf82d6fb
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-17 16:23:15 +0000

    Upload New File

---
+++ Adapters/ContainsDigitPredicateAdapter.cs
@@ -0,0 +1,18 @@
+﻿using ContainsDigitPredicate;
+using PseudoEnumerableTask.Interfaces;
+
+namespace Adapters
+{
+    public class ContainsDigitContainsDigitValidatorAdapter : IPredicate<int>
+    {
+        //TODO: Add necessary code and remove this comment.
+        public ContainsDigitContainsDigitValidatorAdapter(ContainsDigitValidator validator)
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool Verify(int obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
