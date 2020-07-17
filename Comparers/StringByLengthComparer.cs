5b40eec59c05f245c9843d366d2d87a416bbfba4
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-17 16:25:02 +0000

    Upload New File

---
+++ Comparers/StringByLengthComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparers
+{
+    public class StringByLengthComparer : IComparer<string>
+    {
+        public int Compare(string x, string y) => throw new NotImplementedException();
+    }
+}
