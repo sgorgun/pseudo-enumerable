2f5811eaccefd35f30553fbc0ac08439de89c903
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-17 16:24:49 +0000

    Upload New File

---
+++ Comparers/IntegerByAbsComparer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparers
+{
+    public class IntegerByAbsComparer : IComparer<int>
+    {
+        public int Compare(int x, int y) => throw new NotImplementedException();
+    }
+}
